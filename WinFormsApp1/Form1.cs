0216cec8461c5897fce90ac16f7969428a686a0f
OverIQ429 <67504104+OverIQ429@users.noreply.github.com>
2024-12-06 12:18:28 +0300

    Готово

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -7,6 +7,9 @@ using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 using System.Threading;
 using System.Reflection;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+using System.Diagnostics.Eventing.Reader;
+using System.Threading.Channels;
 
 namespace WinFormsApp1
 {
@@ -30,7 +33,7 @@ namespace WinFormsApp1
                     myImage = new Bitmap(openFileDialog.FileName);
                     FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
                     double fileSizeInMB = fileInfo.Length / (1024.0 * 1024.0);
-                    labelSize.Text += $" Размер файла: {fileSizeInMB:F2} MB";
+                    labelSize.Text = $" Размер файла: {fileSizeInMB:F2} MB";
                 }
                 ImageFormat format = myImage.RawFormat;
                 pictureBox1.Image = myImage;
@@ -43,6 +46,7 @@ namespace WinFormsApp1
             {
                 using (SaveFileDialog saveFileDialog = new SaveFileDialog())
                 {
+                    string compressedImagePath = "compressed_image.jpg";
                     saveFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
                     saveFileDialog.Title = "Сохраните изображение";
                     saveFileDialog.FileName = "image";
@@ -69,6 +73,8 @@ namespace WinFormsApp1
             labelTrackBarValue.Text = $"{trackBar1.Value}";
         }
 
+
+
         private void ReduceImageQuality(Bitmap originalImage, int qualityFactor)
         {
             int width = originalImage.Width;
@@ -77,10 +83,6 @@ namespace WinFormsApp1
             double[,] redChannel = new double[height, width]; //Массивы для хранения значений цветовых каналов
             double[,] greenChannel = new double[height, width];
             double[,] blueChannel = new double[height, width];
-            int type0 = 0;
-            int type1 = 1;
-            int type2 = 2;
-            int type3 = 3;
             async Task manytorrent(int type)
             {
                 for (int y = type; y < height; y += 4)
@@ -88,77 +90,83 @@ namespace WinFormsApp1
                     for (int x = 0; x < width; x++)
                     {
                         Color pixel = originalImage.GetPixel(x, y);
-
                         redChannel[y, x] = pixel.R;
                         greenChannel[y, x] = pixel.G;
                         blueChannel[y, x] = pixel.B;
-
                     }
                 }
             }
-            async void StartProcessing()
+            async Task StartProcessing()
             {
                 Task[] tasks = new Task[]
                 {
-                    manytorrent(type0),
-                    manytorrent(type1),
-                    manytorrent(type2),
-                    manytorrent(type3)
+                    manytorrent(0),
+                    manytorrent(1),
+                    manytorrent(2),
+                    manytorrent(3)
                 };
                 await Task.WhenAll(tasks);
+                
             }
-
-            //Thread thr0 = new Thread(manytorrent0);
-            //Thread thr1 = new Thread(manytorrent1);
-            //Thread thr2 = new Thread(manytorrent2);
-            //Thread thr3 = new Thread(manytorrent3);
-            //thr1.Start();
-            //thr2.Start();
-            //thr3.Start();
-            //thr0.Start();
-            StartProcessing();
-            var redMatrix = DenseMatrix.OfArray(redChannel);
-            var greenMatrix = DenseMatrix.OfArray(greenChannel);
-            var blueMatrix = DenseMatrix.OfArray(blueChannel); //создаються матрицы
-            var redSvd = redMatrix.Svd(true);
-            var greenSvd = greenMatrix.Svd(true);
-            var blueSvd = blueMatrix.Svd(true); //Сингулярное разложение
-            //сжатие цветового канала
-            int numComponents = Math.Min(redSvd.S.Count, Math.Min(greenSvd.S.Count, blueSvd.S.Count)) * qualityFactor / 100;
-            var compressedRed = redSvd.U.SubMatrix(0, height, 0, numComponents) *
-                    DenseMatrix.OfDiagonalArray(numComponents, numComponents, redSvd.S.SubVector(0, numComponents).ToArray()) *
-                    redSvd.VT.SubMatrix(0, numComponents, 0, width);
-
-            var compressedGreen = greenSvd.U.SubMatrix(0, height, 0, numComponents) *
-                      DenseMatrix.OfDiagonalArray(numComponents, numComponents, greenSvd.S.SubVector(0, numComponents).ToArray()) *
-                      greenSvd.VT.SubMatrix(0, numComponents, 0, width);
-
-            var compressedBlue = blueSvd.U.SubMatrix(0, height, 0, numComponents) *
-                     DenseMatrix.OfDiagonalArray(numComponents, numComponents, blueSvd.S.SubVector(0, numComponents).ToArray()) *
-                     blueSvd.VT.SubMatrix(0, numComponents, 0, width);
-
-            Bitmap compressedImage = new Bitmap(width, height);
-
-            for (int y = 0; y < height; y++)
+            static int Change_qualiti(Vector<double> singularValues, double qualityFactor)
             {
-                for (int x = 0; x < width; x++)
+                double totalEnergy = singularValues.Sum();
+                double targetEnergy = totalEnergy * (qualityFactor / 100.0);
+                var array_singularValues = singularValues.ToArray();
+                double currentEnergy = 0;
+                int k = 0;
+
+                while (k < array_singularValues.Length && currentEnergy < targetEnergy)
                 {
-                    // Получаем значения пикселей, округляя до целых
-                    int r = (int)Math.Clamp(compressedRed[y, x], 0, 255);
-                    int g = (int)Math.Clamp(compressedGreen[y, x], 0, 255);
-                    int b = (int)Math.Clamp(compressedBlue[y, x], 0, 255);
-
-                    // Устанавливаем пиксель в новое изображение
-                    compressedImage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    currentEnergy += singularValues[k];
+                    k++;
                 }
-            }
-            string compressedImagePath = "compressed_image.jpg";
-            FileInfo fileInfo = new FileInfo(compressedImagePath);
-            compressedImage.Save("compressed_image.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
-            double fileSizeInMB = fileInfo.Length / (1024.0 * 1024.0);
-            labelSize.Text += $" Размер файла: {fileSizeInMB:F2} MB";
 
-            pictureBox1.Image = compressedImage;
+                return Math.Max(1, k);
+            }
+            StartProcessing();
+            async Task<Matrix<double>> Matrix_Arrays_Think(double [,] channel)
+            {
+                var Matrix_Think = DenseMatrix.OfArray(channel);
+                var Svd = Matrix_Think.Svd(true);
+                int k = Change_qualiti(Svd.S, qualityFactor);
+                var U_k = Svd.U.SubMatrix(0, Svd.U.RowCount, 0, k);
+                var S_k = DenseMatrix.OfDiagonalArray(k, k, Svd.S.SubVector(0, k).ToArray());
+                var Vt_k = Svd.VT.SubMatrix(0, k, 0, Svd.VT.ColumnCount);
+                var compressed = U_k * S_k * Vt_k;
+                return compressed;
+            }
+            async void StartMatrixProcessing()
+            {
+                var compressedRed = await Matrix_Arrays_Think(redChannel);
+                var compressedGreen = await Matrix_Arrays_Think(greenChannel);
+                var compressedBlue = await Matrix_Arrays_Think(blueChannel);
+                Bitmap compressedImage = new Bitmap(width, height);
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        // Получаем значения пикселей, округляя до целых
+                        int r = (int)Math.Clamp(compressedRed[y, x], 0, 255);
+                        int g = (int)Math.Clamp(compressedGreen[y, x], 0, 255);
+                        int b = (int)Math.Clamp(compressedBlue[y, x], 0, 255);
+
+                        // Устанавливаем пиксель в новое изображение
+                        compressedImage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+                
+
+                string compressedImagePath = "compressed_image.jpg";
+                FileInfo fileInfo = new FileInfo(compressedImagePath);
+                compressedImage.Save("compressed_image.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                double fileSizeInMB = fileInfo.Length / (1024.0 * 1024.0);
+                labelSize.Text = $" Размер файла: {fileSizeInMB:F2} MB";
+
+                pictureBox1.Image = compressedImage;
+            }
+            StartMatrixProcessing();
         }
         private void aprowdbutton_Click(object sender, EventArgs e)
         {
